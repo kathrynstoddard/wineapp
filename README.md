9dfc949db6394c577ef19c0a42e9530956dcf234
Kathryn Stoddard <kathryn.stoddard@gmail.com>
2014-04-05 14:18:08 -0700

    Adding filestructure for wine tracking app

---
+++ README.md
@@ -0,0 +1,12 @@
+Wine Tracking Web App - Development In progress
+
+This web app helps keep track of wines either in your current collection or past wines that you have liked.
+
+1. Track wines in personal collection
+
+2. Track wines you have tasted in the past
+-Rate wines
+-Make comments on color, taste, etc.
+
+3. Search for Wine ratings
+
---
+++ public/main.js
@@ -0,0 +1,35 @@
+requirejs.config({
+	urlRoot: '/js',
+
+	paths: {
+		'jquery'		: 'lib/jquery-2.1.0',
+		'backbone'	: 'lib/backbone',
+		'underscore': 'lib/underscore',
+		'thorax'		: 'lib/thorax',
+		'hbs'				: 'lib/require-handlebars-plugin/hbs',
+		'templates'	: '../templates',
+		'moment'		: 'lib/moment.min',
+		'bootstrap'	: 'lib/bootstrap.min',
+		'd3'				: 'lib/d3.min' 
+	},
+
+	shim: {
+		'thorax': {
+			deps: ['backbone', 'lib/globalHandlebars'],
+			exports: 'Thorax'
+		}
+	},
+		'bootstrap': {
+			deps: ['jquery']
+		}
+	},
+
+	hbs: {
+		compileOptions: {
+			data: true
+		}
+	}
+
+});
+
+require(['bootstrap','app']);
